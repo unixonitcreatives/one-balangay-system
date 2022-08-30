a6fa8ce73dd37d4bc8c91ab27cc7287860ae5e0f
vincecachi <vincentcachila@gmail.com>
2022-08-30 21:31:28 +0800

    asd

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -28,7 +28,7 @@
 			          <div class="col-sm-6">
 			            <ol class="breadcrumb float-sm-right">
 			              <!-- <li class="breadcrumb-item"><a href="#">Home</a></li> -->
-			              <li class="breadcrumb-item active">Brgy. Canlubang, Calamba City, Laguna</li>
+			              <li class="breadcrumb-item active">Brgy. 46, Pasay City, NCR</li>
 			            </ol>
 			          </div><!-- /.col -->
 			        </div><!-- /.row -->
