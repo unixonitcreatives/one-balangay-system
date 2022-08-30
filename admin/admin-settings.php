9e63006e8a51e5ad1a0554868f1194aa9fe2ea5f
vincecachi <vincentcachila@gmail.com>
2022-08-31 00:03:51 +0800

    Update admin-settings.php

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -65,8 +65,17 @@
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="float-left">Profile Setting</h3>
+									</div>
+									<div class="card-body">
 
-								asd
+										asd
+									</div>
+								</div>
+
+								
 							</div>
 						</div>
 					</div>
