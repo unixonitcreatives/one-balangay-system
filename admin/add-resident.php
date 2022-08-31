548e591a3aefab2bb6aa0a78732c551e4f2a055e
vincecachi <vincentcachila@gmail.com>
2022-08-31 14:43:20 +0800

    Update add-resident.php

--- admin/add-resident.php
+++ admin/add-resident.php
@@ -46,7 +46,7 @@
 						<div class="row">
 
 
-						      	<div class="col-md-12">
+						      	<div class="col-md-8">
 						      		<div class="card card-default">
 						      			<div class="card-header">
 												<h5><i class="fa fa-info-circle"></i> Personal Information</h5>
